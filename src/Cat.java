c8702642c6cc6551f9365b17c6e6815533da3f45
Joss Chapman <jchapman@mymail.mines.edu>
2019-02-21 11:57:44 -0700

    Changed face color to brown

--- src/Cat.java
+++ src/Cat.java
@@ -27,7 +27,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.red);
+		g2.setColor(new Color(128,64,0));
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.green);
@@ -42,7 +42,7 @@ public class Cat {
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
-		// Meow text appears below cat head, +10 places below 
+		// Woof text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("woof", catX, catY+HEAD_DIMENSION+10);	
 	}
